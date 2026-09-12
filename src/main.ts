f7e28d06f06ed901476b2e91db4eafb065d82628
LucaSs55 <lucassantos2018d@gmail.com>
2026-09-12 17:48:41 -0300

    chore: environment config to enable swagger decorators and global pipe validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  const configService = app.get(ConfigService);
+  const env = configService.get<string>('NODE_ENV');
+  const port = configService.get<number>('PORT') ?? 3000;
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  if (env !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle("Fundamentals Study")
+      .setDescription("Fundamentos de Boas práticas de Back-end")
+      .setVersion("1.0")
+      .addTag("Boas Práticas com Regras de Negócio")
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/docs', app, document);
+  }
+
+  await app.listen(port);
+  
+  if (env !== 'production') {
+    console.log(`🚀 Aplicação rodando em: http://localhost:${port}`);
+    console.log(`📄 Swagger ativo em: http://localhost:${port}/api/docs`);
+  }
 }
 bootstrap();
